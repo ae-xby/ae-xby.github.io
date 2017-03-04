80387db076c37f1109fad8884b8324d8fd486692
yxy <yxy.870@gmail.com>
2017-03-04 15:59:37 +0800

    增加公司信息的配置
    Signed-off-by: yxy <yxy.870@gmail.com>

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,12 @@
+#-*- coding: utf-8 -*-
+
 import os.path
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 root = lambda x: os.path.join(PROJECT_PATH, x)
 
 SRC_PATH = root('src')
-OUTPUT_PATH = ('/tmp/xby-output')
+OUTPUT_PATH = root('output')
 TEMPLATE_PATH = root('templates')
 ASSETS_PATH = root('assets')
 DOCS_PATH = root('docs')
@@ -13,3 +15,7 @@ BASE_URL = 'https://www.xiaobaiyangyixun.com'
 LANGUAGE = "cn"
 THEME_COLOR = 'blue'
 STYLE_SWITCHER_ON = True
+
+ADDRESS = u'广州市海珠区昌岗江南坊5楼'
+TEL = "020 1234 123"
+EMAIL = "info@xby.com"
